0deb696e60e029272fdfafdc4cffe1f655ec6704
Josef Nhidi <josef@nhidi.com>
2026-03-25 15:25:49 +0100

    Add missing Log facade import

--- backend/app/Http/Controllers/Api/PhotoController.php
+++ backend/app/Http/Controllers/Api/PhotoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
